73a3de31f6f3735213c8f51b751e2ca8fa688fc6
Bonerush <2630234655@qq.com>
2026-05-18 21:29:30 +0800

    feat: port ui_core animation engine and main loop

---
+++ src/ui/ui_core.h
@@ -0,0 +1,27 @@
+#ifndef UI_CORE_H
+#define UI_CORE_H
+
+#include <stdbool.h>
+
+#define ALLOW_EXIT_ASTRA_UI_BY_USER 1
+
+extern bool in_astra;
+
+extern void ad_astra(void);
+extern bool astra_is_in_user_item(void);
+extern void astra_refresh_info_bar(void);
+extern void astra_refresh_pop_up(void);
+extern void astra_refresh_camera_position(void);
+extern void astra_refresh_widget_core_position(void);
+extern void astra_init_list(void);
+extern void astra_init_core(void);
+extern void astra_refresh_list_item_position(void);
+extern void astra_refresh_selector_position(void);
+extern void astra_refresh_main_core_position(void);
+extern void astra_ui_widget_core(void);
+extern void astra_ui_main_core(void);
+
+extern void astra_animation(float *_pos, float _posTrg, float _speed);
+extern uint8_t astra_exit_animation_status;
+
+#endif
